8272760eccaabb9f838c7c22710b781aa218cfd3
Venzi Petkov <venzi.petkov@googlemail.com>
2016-10-14 15:50:36 +0200

    making parmutation

--- src/vp/util/math/comb/Permutation.java
+++ src/vp/util/math/comb/Permutation.java
@@ -5,10 +5,33 @@
  */
 package vp.util.math.comb;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author venzipetkov
  */
 public class Permutation {
-    
+   public LinkedList<Integer> getPermutation(PermutationCode pc){
+       LinkedList<Integer> result = new LinkedList<Integer>();
+       int counter = 1;
+       for (Iterator iterator = pc.code.iterator(); iterator.hasNext();) {
+           Integer next = (Integer)iterator.next();
+           result.add(next - 1, counter);
+           counter++; 
+       }
+       return result;
+   }
+   
+   public List permuteList(List list, PermutationCode pc){
+       List result = new LinkedList();
+       LinkedList<Integer> permutationOrigin = getPermutation(pc);
+       for (Iterator iterator = permutationOrigin.iterator(); iterator.hasNext();) {
+           Integer next = (Integer)iterator.next();
+           result.add(list.get(next-1));
+       }
+       return list;
+   }
 }
---
+++ test/vp/util/math/comb/PermutationTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vp.util.math.comb;
+
+import java.util.LinkedList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author venzipetkov
+ */
+public class PermutationTest {
+    
+    public PermutationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPermutation method, of class Permutation.
+     */
+    @Test
+    public void testGetPermutation() {
+         try{
+//             Initial permutation
+            PermutationCode pc = new PermutationCode(3);
+            Permutation instance = new Permutation();
+            LinkedList<Integer> list = instance.getPermutation(pc);
+            Object[] perm = list.toArray();
+            int[] expected = {3,2,1};
+            
+            assertEquals(expected[2], perm[2]);
+            assertEquals(expected[1], perm[1]);
+            assertEquals(expected[0], perm[0]);
+
+//            next permutation            
+            
+            pc = PermutationCode.generateNextCode(pc);
+            list = instance.getPermutation(pc);
+            perm = list.toArray();
+            int[] expected2 = {2,3,1};
+            
+            assertEquals(expected2[2], perm[2]);
+            assertEquals(expected2[1], perm[1]);
+            assertEquals(expected2[0], perm[0]);
+            
+//            inverted permutation            
+            pc = PermutationCode.inverseCode(pc);
+            list = instance.getPermutation(pc);
+            perm = list.toArray();
+            int[] expected3 = {1,3,2};
+            
+            assertEquals(expected3[2], perm[2]);
+            assertEquals(expected3[1], perm[1]);
+            assertEquals(expected3[0], perm[0]);
+            
+        }
+        catch (Exception e){
+            fail("Initial permutation failed ");
+        }
+    }
+    
+}
